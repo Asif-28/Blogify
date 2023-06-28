a4ec58ab200de993122b82a3e320f5bf564f94da
Asif Ali <asif.a.7277@gmail.com>
2023-06-28 13:14:30 +0530

    hero section added

--- src/comp/Hero.jsx
+++ src/comp/Hero.jsx
@@ -3,7 +3,21 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="bg-[url('../../public/assets/b.jpg')] bg-center bg-cover min-h-[100vh] brightness-50"></div>
+    <main>
+      <div className="bg-[url('../../public/assets/b.jpg')] bg-center bg-cover min-h-[100vh] brightness-50 relative"></div>
+      <div className="detail absolute top-[70%] left-[10%] sm:left-[15%] md:left-[35%] items flex flex-col items-center gap-5 ">
+        <input
+          type="text"
+          className="py-2 sm:py-3 pl-4 border-none pr-4 w-[300px] sm:min-w-[400px] text-black rounded-full font-light text-center"
+          placeholder="Share Your Views with Us!! "
+        />
+        <div className="btn">
+          <button className="bg-[#fdef32] text-lg sm:text-xl text-black py-3 sm:py-4 px-8 sm:px-12 rounded-full  font-normal">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </main>
   );
 };
 
