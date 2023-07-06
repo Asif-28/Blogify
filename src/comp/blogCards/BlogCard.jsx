e38aa41c84edc59b029e1407f191f796b17f6a7f
Asif Ali <asif.a.7277@gmail.com>
2023-07-06 22:40:53 +0530

    changes in the theme

--- src/comp/blogCards/BlogCard.jsx
+++ src/comp/blogCards/BlogCard.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const BlogCard = ({ img, title, desc, authImg, author, date }) => {
   return (
     <div className="max-w-[1280px]  mx-auto mb-[20px]">
-      <div className="card max-w-[410px] mb-[1rem] sm:mb-[1.5rem] bg-[#1d1e1f] rounded-md flex flex-col overflow-clip justify-center border border-[##e5eaf3] mx-2 transform origin-bottom transition-transform duration-200 hover:scale-y-[1.02] ">
+      <div className="card max-w-[410px] mb-[1rem] sm:mb-[1.5rem] bg-[#1d1e1f] rounded-md flex flex-col overflow-clip justify-center border border-[##e5eaf3] mx-2 transform origin-bottom transition-transform duration-200 md:hover:scale-y-[1.02]  ">
         <div className="img">
           <img
             src={img}
             alt="blog-img"
-            className="min-h-[220px] sm:max-h-[230px] w-[100%] object-cover"
+            className="min-h-[200px] sm:max-h-[230px] w-[100%] object-cover"
           />
         </div>
         <div className="content p-[1.5rem] max-h-[230px]">
-          <h3 className="text-[1.5rem] font-light overflow-hidden leading-6 ">
+          <h3 className="text-[1.5rem] font-light overflow-hidden leading-6 md:hover:text-[#009191]">
             {title}
           </h3>
           <p className="mt-[1.5rem] text-[.875rem] ">{desc}</p>
