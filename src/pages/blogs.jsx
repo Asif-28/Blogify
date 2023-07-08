207dd410a4436e8d0db2588e921caa01614bd4ae
Asif Ali <asif.a.7277@gmail.com>
2023-07-08 23:08:44 +0530

    demo str of the articles and blogs

---
+++ src/pages/blogs.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+
+const Blogs = () => {
+  return (
+    <div>
+      <div className="container mt-[80px] max-w-[1080px] mx-auto ]">
+        <div className="article flex gap-4 bg-[#0a0a0a] p-4 rounded-xl ">
+          <div className="left pr-12">
+            <h1 className="text-2xl ">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam,
+              rem.
+            </h1>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum cum
+              odit totam ea quia dignissimos enim rem at illum corporis.
+            </p>
+          </div>
+          <div className="right">
+            <div className="img">
+              <img
+                src="https://miro.medium.com/v2/resize:fill:250:168/1*RAthRIkRXYs8anYtioLvIw.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="article flex gap-4 bg-[#0a0a0a] p-4 rounded-xl mt-[.5rem]">
+          <div className="left pr-12">
+            <h1 className="text-2xl ">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam,
+              rem.
+            </h1>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum cum
+              odit totam ea quia dignissimos enim rem at illum corporis.
+            </p>
+          </div>
+          <div className="right">
+            <div className="img">
+              <img
+                src="https://miro.medium.com/v2/resize:fill:250:168/1*RAthRIkRXYs8anYtioLvIw.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="article flex gap-4 bg-[#0a0a0a] p-4 rounded-xl mt-[.5rem]">
+          <div className="left pr-12">
+            <h1 className="text-2xl ">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam,
+              rem.
+            </h1>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum cum
+              odit totam ea quia dignissimos enim rem at illum corporis.
+            </p>
+          </div>
+          <div className="right">
+            <div className="img">
+              <img
+                src="https://miro.medium.com/v2/resize:fill:250:168/1*RAthRIkRXYs8anYtioLvIw.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="article flex gap-4 bg-[#0a0a0a] p-4 rounded-xl mt-[.5rem]">
+          <div className="left pr-12">
+            <h1 className="text-2xl ">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam,
+              rem.
+            </h1>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum cum
+              odit totam ea quia dignissimos enim rem at illum corporis.
+            </p>
+          </div>
+          <div className="right">
+            <div className="img">
+              <img
+                src="https://miro.medium.com/v2/resize:fill:250:168/1*RAthRIkRXYs8anYtioLvIw.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="article flex gap-4 bg-[#0a0a0a] p-4 rounded-xl mt-[.5rem]">
+          <div className="left pr-12">
+            <h1 className="text-2xl ">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam,
+              rem.
+            </h1>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum cum
+              odit totam ea quia dignissimos enim rem at illum corporis.
+            </p>
+          </div>
+          <div className="right">
+            <div className="img">
+              <img
+                src="https://miro.medium.com/v2/resize:fill:250:168/1*RAthRIkRXYs8anYtioLvIw.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blogs;
