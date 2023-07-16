857d2d0f638b1c131c0ba8bc5fd065c540dcb120
Asif Ali <asif.a.7277@gmail.com>
2023-07-16 22:41:24 +0530

    removed the deployment erroe

--- src/pages/myBlogs.jsx
+++ src/pages/myBlogs.jsx
@@ -3,9 +3,11 @@ import CreateBlog from "@/comp/createBlog/CreateBlog";
 import { useSession } from "next-auth/react";
 import React from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
+
 const MyBlogs = () => {
-  const { data: session } = useSession();
   const router = useRouter();
+  const { data: session } = useSession();
   if (session) {
     const { user } = session;
     const name = user.name;
@@ -16,7 +18,22 @@ const MyBlogs = () => {
       </div>
     );
   } else {
-    router.push("./login");
+    return (
+      <div className="login ">
+        <div className="login-first pl-4 flex sm:justify-center  bg-[#f0ba15] min-h-[calc(100vh-65px)] mt-[65px]">
+          <div className="login  text-black">
+            <h1 className="text-2xl pt-2 font-light">
+              Login to create your blogs
+            </h1>
+            <Link className="" href="./login">
+              <button className="min-w-[80px] px-3 bg-[#0a0a0a] text-[#fff] py-2 mt-4">
+                Login
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
   }
 };
 
