7ba1b17dafa5df16ed142df98ec3e5762bfef5ae
Asif-28 <asif.a.7277@gmail.com>
2023-08-20 23:06:56 +0530

    header mobile responsive hamburger

--- src/comp/header-footer/Header.jsx
+++ src/comp/header-footer/Header.jsx
@@ -1,15 +1,23 @@
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
-import { signOut, useSession } from "next-auth/react";
+
+'use client';
+import { useEffect, useState } from 'react';
+import React from 'react';
+// import Image from 'next/image';
+import Link from 'next/link';
+import DensitySmallIcon from '@mui/icons-material/DensitySmall';
 // import Logout from "./Logout";
+import { signOut, useSession } from "next-auth/react";
 
 const Logout = () => {
   return <button onClick={signOut}>Logout</button>;
 };
 
 const Header = () => {
-  const [show, handleShow] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [show, handleShow] = useState(false);
   const { data: session } = useSession();
+
+
   const transitionNavBar = () => {
     if (window.scrollY > 100) {
       handleShow(true);
@@ -18,21 +26,31 @@ const Header = () => {
     }
   };
 
+
   useEffect(() => {
     window.addEventListener("scroll", transitionNavBar);
     return () => window.removeEventListener("scroll", transitionNavBar);
   }, []);
+
   return (
-    <div className={`nav ${show && `nav__black`}`}>
+    <div className="relative">
+      <div
+        className="fixed top-2 left-2 backdrop-blur-sm z-50 p-2 mt-1 mr-3 text-sm text-white rounded-lg sm:hidden"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        <DensitySmallIcon className='text-2xl' />
+      </div>
+      
+   <div className={`nav ${show && `nav__black`}`}>
       <div className="container justify-between px-3  sm:px-6 flex max-w-[1280px] mx-auto ">
-        <div className="logo sm:flex-1 text-2xl sm:text-2xl ">
-          <img
+        <div className="logo pl-8 sm:pl-0 ">
+           <img
             src="./assets/logo1.svg"
-            className="h-[35px] sm:h-[40px] w-[55px] sm:w-[100px]"
+            className="h-[40px] sm:h-[50px] w-[80px] sm:w-[100px]"
             alt=""
           />
         </div>
-        <div className="flex sm:flex-1 gap-8 ">
+        <div className="hidden  sm:flex sm:flex-1 gap-8 ">
           <div className="flex-1 ">
             <ul className="flex justify-center gap-2 sm:gap-6 text-[17px] sm:text-[20px] sm:font-normal ">
               <li className="sm:hover:border sm:hover-border-[#e5eaf3] sm:px-[0.4rem] font-light sm:w-[70px] sm:text-center ">
@@ -43,6 +61,9 @@ const Header = () => {
               </li>
               <li className="sm:hover:border sm:hover-border-[#e5eaf3] sm:px-[0.4rem] font-light sm:w-[90px] sm:text-center">
                 <Link href="./profile">Profile</Link>
+              </li>
+              <li className="sm:hover:border sm:hover-border-[#e5eaf3] sm:px-[0.4rem] font-light sm:w-[150px] sm:text-center">
+                <Link href="./myBlogs">CreateBlogs</Link>
               </li>
             </ul>
           </div>
@@ -56,37 +77,53 @@ const Header = () => {
         </div>
       </div>
     </div>
+  
+      <div
+        style={{
+          transform: isMenuOpen ? 'translateX(0)' : 'translateX(-100%)',
+          transition: 'all 0.3s ease-out',
+        }}
+        className="w-2/3 h-screen bg-[#13171E] text-white fixed top-0 left-0 lg:hidden z-20 px-3 py-0 flex flex-row rounded-br-sm font-semibold text-[20px]"
+      >
+   
+      <div className=" flex flex-col mx-auto ">
+        <div className="logo  text-2xl sm:text-2xl ">
+          <img
+            src="./assets/logo1.svg"
+            className="h-[70px] w-[70px]"
+            alt=""
+          />
+        </div>
+        <div className="flex flex-col pt-8">
+          <div className="">
+            <ul className="flex flex-col justify-center gap-3 sm:gap-6 text-[20px] sm:text-[20px] sm:font-normal ">
+              <li className="sm:hover:border sm:hover-border-[#e5eaf3] sm:px-[0.4rem] font-light sm:w-[70px] sm:text-center ">
+                <Link href="/">Home</Link>
+              </li>
+              <li className="sm:hover:border sm:hover-border-[#e5eaf3] sm:px-[0.4rem] font-light sm:w-[60px] sm:text-center">
+                <Link href="./blogs">Blogs</Link>
+              </li>
+              <li className="sm:hover:border sm:hover-border-[#e5eaf3] sm:px-[0.4rem] font-light sm:w-[90px] sm:text-center">
+                <Link href="./profile">Profile</Link>
+              </li>
+              <li className="sm:hover:border sm:hover-border-[#e5eaf3] sm:px-[0.4rem] font-light sm:w-[150px] sm:text-center">
+                <Link href="./myBlogs">CreateBlogs</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="login pt-8 font-[24px]">
+            <Link href="./login">
+              <li className="text-center list-none font-light border px-[0.2rem] sm:border-[#fff] sm:px-[0.4rem]">
+                {session ? <Logout /> : "Login"}
+              </li>
+            </Link>
+          </div>
+        </div>
+      </div>
+   
+      </div>
+    </div>
   );
 };
 
 export default Header;
-
-// .nav {
-//   position: fixed;
-//   top: 0;
-//   padding: 10px;
-//   width: 100%;
-//   height: 50px;
-//   z-index: 1;
-//   transition-timing-function: ease-in;
-//   transition: all 0.5s;
-// }
-
-// const Header = () => {
-//   const { data: session } = useSession();
-//   return (
-//     <main className={styles.main}>
-//       <div className={styles.logo}>Blogs..</div>
-//       <nav className={styles.items}>
-//         <ul className={styles.item}>
-//           <Link href={`/ `}>
-//             <li>Home</li>
-//           </Link>
-//           <Link href="./credentialsLogin">
-//             <li>{session ? `${session.user.name}` : "Login"}</li>
-//           </Link>
-//         </ul>
-//       </nav>
-//     </main>
-//   );
-// };
